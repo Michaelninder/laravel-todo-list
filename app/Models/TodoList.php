de99d24cf11b770e1c44bac12df1e2c8a719247c
Michaelninder <fabianeternis@gmail.com>
2025-07-27 11:03:49 +0200

    fix migration for TodoList and TodoItem

--- app/Models/TodoList.php
+++ app/Models/TodoList.php
@@ -4,9 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TodoList extends Model
 {
     /** @use HasFactory<\Database\Factories\TodoListFactory> */
     use HasFactory;
+
+    /**
+     * Get the user that owns the TodoList
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all of the items for the TodoList
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function items(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'list_id', 'id');
+    }
 }
